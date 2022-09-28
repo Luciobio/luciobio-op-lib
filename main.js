244eb3149f81bc1f52fbb00aa59b2a3906a3cf4e
Luciano Pardo <pardobio@gmail.com>
2022-09-27 21:49:09 -0300

    after afterClass

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ const items = [
     {
         id: 2,
         name: "Substrate Eden - 25L",
-        price: 1075,
+        price: 3500,
         description: "• EDEN - Queens paradise es un sustrato de uso profesional, que ofrece óptimas condiciones de siembra y/o trasplante, por su excelente aireación, retención de agua, reacción (pH) y Ce.",
         category: "Growing",
-        stock: 20,
+        stock: 0,
         img: './img/eden-substrate.jpg'
     },
     {
@@ -28,6 +28,8 @@ const items = [
     }
 ];
 
+const iva = 0.2
+
 const cart = [];
 
 const cardContainer = document.getElementById('cardContainer');
@@ -35,13 +37,21 @@ const cardContainer = document.getElementById('cardContainer');
 const cartContainer = document.getElementById('cartContainer');
 
 items.forEach(e => {
+    const {
+        name,
+        img,
+        price,
+        stock,
+        id
+    } = e;
     let card = document.createElement('div');
     card.setAttribute('class', 'Item');
     card.innerHTML = `
-        <img  alt=${e.name} src='${e.img}'/>
-        <h4>${e.name}</h4>
-        <h3>$ ${e.price}</h3>
-        <button class='addbtn' id='${e.id}'><a class='whiteLink'>ADD TO CART</a></button>
+        <img  alt=${name} src='${img}'/>
+        <h4>${name}</h4>
+        <h3>$ ${price}</h3>
+        <h3 class=${stock != 0 ? 'green' : 'red'}> Stock:${stock || 'Out of Stock'}</h3>
+        <button class='addbtn' id='${id}'><a class='whiteLink'>ADD TO CART</a></button>
     `;
     cardContainer.appendChild(card);
 });
@@ -56,20 +66,39 @@ for (let btn of buttons) {
 const showCartBtn = document.getElementById('showCart');
 showCartBtn.addEventListener('click', () => showCart());
 
+const checkOut = document.getElementById('checkOut');
+checkOut.addEventListener('click', () => checkOutfn());
+
 // FUNCTIONS
 const addToCart = id => {
-    cart.push(items.find(i => i.id == id));
+    const cartItem = items.find(i => i.id == id);
+    const item4cart = { ...cartItem, stock: cartItem.stock - 1, quantity: 1 }
+    cart.push(item4cart);
     console.log(cart);
+
+    Toastify({
+        text: `${item4cart.name} agregado con éxito`,
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
+    }).showToast();
 };
 
 const showCart = () => {
     if (cart.length === 0) {
-        cartContainer.innerHTML=``;
+        cartContainer.innerHTML = ``;
         let sign = document.createElement('h2');
         sign.innerHTML = `No items in the Cart`;
         cartContainer.appendChild(sign);
     } else {
-        cartContainer.innerHTML=``;
+        cartContainer.innerHTML = ``;
         cart.forEach(e => {
             let cartItem = document.createElement('div');
             cartItem.innerHTML = `
@@ -80,3 +109,12 @@ const showCart = () => {
         })
     }
 }
+
+const checkOutfn = () => {
+    Swal.fire({
+        icon: 'success',
+        title: 'Exito',
+        text: 'Su orden ha sido generada con éxito',
+        footer: '<a href="/">Continuar comprando</a>'
+      })
+};
